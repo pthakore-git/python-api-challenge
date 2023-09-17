75013a398c62a9b1cfb8f411fc79801e978029a0
pthakore <paulinthakore@hotmail.com>
2023-09-17 17:10:42 -0400

    uploading .gitignore file

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "6e4fff24d029c4f2cce6a0af4265f675"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "162860577196443194a1029939ea324d"
